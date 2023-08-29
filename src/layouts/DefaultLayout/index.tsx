d497e9f4b0474a2c20116444c73aa2e4d85586d8
Rafaela Rodrigues <ltcmdrdata@protonmail.com>
2023-08-29 11:19:39 -0300

    feat: bookwise pages and components

---
+++ src/layouts/DefaultLayout/index.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+import { Container, Content } from "./styles"
+import Head from "next/head";
+import { Sidebar } from "@/components/Sidebar";
+
+type DefaultLayoutProps = {
+    children: ReactNode
+    title: string;
+}
+
+export const DefaultLayout = ({ title, children }: DefaultLayoutProps) => {
+    return (
+        <Container>
+            <Head>
+                <title>{`${title} | BookWise`}</title>
+                <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+
+            </Head>
+            <Sidebar />
+            <Content>
+                {children}
+            </Content>
+        </Container>
+    )
+}
